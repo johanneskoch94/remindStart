91f0d4fa59fe4e07a99e941860dbb1308e421b81
Johannes Koch <jokoch@pik-potsdam.de>
2022-03-07 18:39:45 +0100

    Specify package of imported functions

--- R/prepare_NDC.R
+++ R/prepare_NDC.R
@@ -1,22 +1,22 @@
 prepare_NDC <- function(gdx, cfg) {
 
   if (file.exists(gdx)) {
-    emi <- reportEmi(gdx)
+    emi <- remind2::reportEmi(gdx)
   } else {
     stop("No gdx file found - please provide gdx from reference BAU run")
   }
-  regs <- setdiff(getRegions(emi), "GLO")
-  if ("Emi|GHG|w/o Land-Use Change (Mt CO2eq/yr)" %in% getItems(emi, 3.1)) {
+  regs <- setdiff(magclass::getRegions(emi), "GLO")
+  if ("Emi|GHG|w/o Land-Use Change (Mt CO2eq/yr)" %in% magclass::getItems(emi, 3.1)) {
     pm_BAU_reg_emi_wo_LU_bunkers <- emi[regs, seq(2005, 2050, 5), "Emi|GHG|w/o Land-Use Change (Mt CO2eq/yr)"]
-  } else if ("Emi|Kyoto Gases excl Land-Use Change|w/o Bunkers (Mt CO2-equiv/yr)" %in% getItems(emi, 3.1)) {
+  } else if ("Emi|Kyoto Gases excl Land-Use Change|w/o Bunkers (Mt CO2-equiv/yr)" %in% magclass::getItems(emi, 3.1)) {
     pm_BAU_reg_emi_wo_LU_bunkers <- emi[regs, seq(2005, 2050, 5), "Emi|Kyoto Gases excl Land-Use Change|w/o Bunkers (Mt CO2-equiv/yr)"]
   } else {
     stop("No emissions variable found in the NDC script!")
   }
 
-  getNames(pm_BAU_reg_emi_wo_LU_bunkers) <- NULL
-  write.magpie(pm_BAU_reg_emi_wo_LU_bunkers, "./modules/45_carbonprice/NDC/input/pm_BAU_reg_emi_wo_LU_bunkers.cs4r",
-               comment = "** description: Regional GHG emi (excl. LU and bunkers) in BAU scenario \n*** unit: Mt CO2eq/yr \n*** file created with scripts/input/prepare_NDC.R")
-  write.magpie(pm_BAU_reg_emi_wo_LU_bunkers, "./modules/46_carbonpriceRegi/NDC/input/pm_BAU_reg_emi_wo_LU_bunkers.cs4r",
-               comment = "** description: Regional GHG emi (excl. LU and bunkers) in BAU scenario \n*** unit: Mt CO2eq/yr \n*** file created with scripts/input/prepare_NDC.R")
+  magclass::getNames(pm_BAU_reg_emi_wo_LU_bunkers) <- NULL
+  magclass::write.magpie(pm_BAU_reg_emi_wo_LU_bunkers, "./modules/45_carbonprice/NDC/input/pm_BAU_reg_emi_wo_LU_bunkers.cs4r",
+                         comment = "** description: Regional GHG emi (excl. LU and bunkers) in BAU scenario \n*** unit: Mt CO2eq/yr \n*** file created with scripts/input/prepare_NDC.R")
+  magclass::write.magpie(pm_BAU_reg_emi_wo_LU_bunkers, "./modules/46_carbonpriceRegi/NDC/input/pm_BAU_reg_emi_wo_LU_bunkers.cs4r",
+                         comment = "** description: Regional GHG emi (excl. LU and bunkers) in BAU scenario \n*** unit: Mt CO2eq/yr \n*** file created with scripts/input/prepare_NDC.R")
 }
