0604ed12914db8cd7c2494701c69f410328f5f60
Johannes Koch <jokoch@pik-potsdam.de>
2023-08-29 09:41:01 +0200

    Update fixings

--- R/prepare_functions.R
+++ R/prepare_functions.R
@@ -223,6 +223,31 @@ create_standard_fixings <- function(cfg, ref_gdx_data) {
       list(c("q80_budget_helper.M", "!!q80_budget_helper.M"))
     )
   }
+
+  # KK filter out module 39 CCU fixings
+  if (cfg$gms$CCU == "off") {
+    levs_manipulateThis <- c(
+      levs_manipulateThis,
+      list(c("v39_shSynTrans.L", "!!v39_shSynTrans.L")),
+      list(c("v39_shSynGas.L", "!!v39_shSynGas.L"))
+    )
+
+    fixings_manipulateThis <- c(
+      fixings_manipulateThis,
+      list(c("v39_shSynTrans.FX", "!!v39_shSynTrans.FX")),
+      list(c("v39_shSynGas.FX", "!!v39_shSynGas.FX"))
+    )
+
+    margs_manipulateThis <- c(
+      margs_manipulateThis,
+      list(c("v39_shSynTrans.M", "!!v39_shSynTrans.M")),
+      list(c("v39_shSynGas.M", "!!v39_shSynGas.M")),
+      list(c("q39_emiCCU.M", "!!q39_emiCCU.M")),
+      list(c("q39_shSynTrans.M", "!!q39_shSynTrans.M")),
+      list(c("q39_shSynGas.M", "!!q39_shSynGas.M"))
+    )
+  }
+
   # RP filter out module 40 techpol fixings
   if (cfg$gms$techpol == "none") {
     margs_manipulateThis <- c(
@@ -253,27 +278,256 @@ create_standard_fixings <- function(cfg, ref_gdx_data) {
     )
   }
 
-  levs_manipulateThis <- c(
-    levs_manipulateThis,
-    list(c("vm_shBioFe.L", "!!vm_shBioFe.L"))
-  )
-  fixings_manipulateThis <- c(
-    fixings_manipulateThis,
-    list(c("vm_shBioFe.FX", "!!vm_shBioFe.FX"))
-  )
-  margs_manipulateThis <- c(
-    margs_manipulateThis,
-    list(c("vm_shBioFe.M", "!!vm_shBioFe.M"))
-  )
+  # KK CDR module realizations
+ if (cfg$gms$CDR == "DAC") {
+   fixings_manipulateThis <- c(
+     fixings_manipulateThis,
+     list(c("v33_emiEW.FX", "!!v33_emiEW.FX")),
+     list(c("v33_grindrock_onfield.FX", "!!v33_grindrock_onfield.FX")),
+     list(c("v33_grindrock_onfield_tot.FX", "!!v33_grindrock_onfield_tot.FX"))
+   )
 
-  # RP filter out regipol items
-  if (grepl("off", cfg$gms$cm_implicitFE, ignore.case = T)) {
-    margs_manipulateThis <- c(
-      margs_manipulateThis,
-      list(c("q47_implFETax.M", "!!q47_implFETax.M"))
-    )
-  }
+   levs_manipulateThis <- c(
+     levs_manipulateThis,
+     list(c("v33_emiEW.L", "!!v33_emiEW.L")),
+     list(c("v33_grindrock_onfield.L", "!!v33_grindrock_onfield.L")),
+     list(c("v33_grindrock_onfield_tot.L", "!!v33_grindrock_onfield_tot.L"))
+   )
 
+   margs_manipulateThis <- c(
+     margs_manipulateThis,
+     list(c("v33_emiEW.M", "!!v33_emiEW.M")),
+     list(c("v33_grindrock_onfield.M", "!!v33_grindrock_onfield.M")),
+     list(c("v33_grindrock_onfield_tot.M", "!!v33_grindrock_onfield_tot.M")),
+     list(c("q33_capconst_grindrock.M", "!!q33_capconst_grindrock.M")),
+     list(c("q33_grindrock_onfield_tot.M", "!!q33_grindrock_onfield_tot.M")),
+     list(c("q33_omcosts.M", "!!q33_omcosts.M")),
+     list(c("q33_potential.M", "!!q33_potential.M")),
+     list(c("q33_emiEW.M", "!!q33_emiEW.M")),
+     list(c("q33_LimEmiEW.M", "!!q33_LimEmiEW.M"))
+   )
+ }
+
+ if (cfg$gms$CDR == "weathering") {
+   fixings_manipulateThis <- c(
+     fixings_manipulateThis,
+     list(c("v33_emiDAC.FX", "!!v33_emiDAC.FX")),
+     list(c("v33_DacFEdemand_el.FX", "!!v33_DacFEdemand_el.FX")),
+     list(c("v33_DacFEdemand_heat.FX", "!!v33_DacFEdemand_heat.FX"))
+   )
+
+   levs_manipulateThis <- c(
+     levs_manipulateThis,
+     list(c("v33_emiDAC.L", "!!v33_emiDAC.L")),
+     list(c("v33_DacFEdemand_el.L", "!!v33_DacFEdemand_el.L")),
+     list(c("v33_DacFEdemand_heat.L", "!!v33_DacFEdemand_heat.L"))
+   )
+
+   margs_manipulateThis <- c(
+     margs_manipulateThis,
+     list(c("v33_emiDAC.M", "!!v33_emiDAC.")),
+     list(c("v33_DacFEdemand_el.M", "!!v33_DacFEdemand_el.M")),
+     list(c("v33_DacFEdemand_heat.M", "!!v33_DacFEdemand_heat.M")),
+     list(c("q33_DacFEdemand_heat.M", "!!q33_DacFEdemand_heat.M")),
+     list(c("q33_DacFEdemand_el.M", "!!q33_DacFEdemand_el.M")),
+     list(c("q33_capconst_dac.M", "!!q33_capconst_dac.M")),
+     list(c("q33_ccsbal.M", "!!q33_ccsbal.M")),
+     list(c("q33_H2bio_lim.M", "!!q33_H2bio_lim.M"))
+   )
+ }
+
+ if (cfg$gms$CDR == "off") {
+   fixings_manipulateThis <- c(
+     fixings_manipulateThis,
+     list(c("v33_emiDAC.FX", "!!v33_emiDAC.FX")),
+     list(c("v33_emiEW.FX", "!!v33_emiEW.FX")),
+     list(c("v33_DacFEdemand_el.FX", "!!v33_DacFEdemand_el.FX")),
+     list(c("v33_DacFEdemand_heat.FX", "!!v33_DacFEdemand_heat.FX")),
+     list(c("v33_grindrock_onfield.FX", "!!v33_grindrock_onfield.FX")),
+     list(c("v33_grindrock_onfield_tot.FX", "!!v33_grindrock_onfield_tot.FX"))
+   )
+
+   levs_manipulateThis <- c(
+     levs_manipulateThis,
+     list(c("v33_emiDAC.L", "!!v33_emiDAC.L")),
+     list(c("v33_emiEW.L", "!!v33_emiEW.L")),
+     list(c("v33_DacFEdemand_el.L", "!!v33_DacFEdemand_el.L")),
+     list(c("v33_DacFEdemand_heat.L", "!!v33_DacFEdemand_heat.L")),
+     list(c("v33_grindrock_onfield.L", "!!v33_grindrock_onfield.L")),
+     list(c("v33_grindrock_onfield_tot.L", "!!v33_grindrock_onfield_tot.L"))
+   )
+
+   margs_manipulateThis <- c(
+     margs_manipulateThis,
+     list(c("v33_emiDAC.M", "!!v33_emiDAC.M")),
+     list(c("v33_emiEW.M", "!!v33_emiEW.M")),
+     list(c("v33_grindrock_onfield.M", "!!v33_grindrock_onfield.M")),
+     list(c("v33_grindrock_onfield_tot.M", "!!v33_grindrock_onfield_tot.M")),
+     list(c("v33_DacFEdemand_el.M", "!!v33_DacFEdemand_el.M")),
+     list(c("v33_DacFEdemand_heat.M", "!!v33_DacFEdemand_heat.M")),
+     list(c("q33_capconst_grindrock.M", "!!q33_capconst_grindrock.M")),
+     list(c("q33_grindrock_onfield_tot.M", "!!q33_grindrock_onfield_tot.M")),
+     list(c("q33_omcosts.M", "!!q33_omcosts.M")),
+     list(c("q33_potential.M", "!!q33_potential.M")),
+     list(c("q33_emiEW.M", "!!q33_emiEW.M")),
+     list(c("q33_LimEmiEW.M", "!!q33_LimEmiEW.M")),
+     list(c("q33_DacFEdemand_heat.M", "!!q33_DacFEdemand_heat.M")),
+     list(c("q33_DacFEdemand_el.M", "!!q33_DacFEdemand_el.M")),
+     list(c("q33_capconst_dac.M", "!!q33_capconst_dac.M")),
+     list(c("q33_ccsbal.M", "!!q33_ccsbal.M")),
+     list(c("q33_H2bio_lim.M", "!!q33_H2bio_lim.M")),
+     list(c("q33_demFeCDR.M", "!!q33_demFeCDR.M")),
+     list(c("q33_emicdrregi.M", "!!q33_emicdrregi.M")),
+     list(c("q33_otherFEdemand.M", "!!q33_otherFEdemand.M"))
+   )
+ }
+ # end of CDR module realizations
+
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("vm_shBioFe.L", "!!vm_shBioFe.L"))
+ )
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("vm_shBioFe.FX", "!!vm_shBioFe.FX"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("vm_shBioFe.M", "!!vm_shBioFe.M")),
+   list(c("q39_EqualSecShare_BioSyn.M", "!!q39_EqualSecShare_BioSyn.M"))
+ )
+
+ # OR: renamed for sectoral taxation
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("vm_emiCO2_sector.L", "vm_emiCO2Sector.L")),
+   list(c("v21_taxrevCO2_sector.L", "v21_taxrevCO2Sector.L"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("vm_emiCO2_sector.M", "vm_emiCO2Sector.M")),
+   list(c("v21_taxrevCO2_sector.M", "v21_taxrevCO2Sector.M")),
+   list(c("q_emiCO2_sector.M", "q_emiCO2Sector.M")),
+   list(c("q21_taxrevCO2_sector.M", "q21_taxrevCO2Sector.M"))
+ )
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("vm_emiCO2_sector.FX", "vm_emiCO2Sector.FX")),
+   list(c("v21_taxrevCO2_sector.FX", "v21_taxrevCO2Sector.FX"))
+ )
+
+ # renamed because of https://github.com/remindmodel/remind/pull/796
+ manipulate_tradesets <- c(
+   list(c("'gas_pipe'", "'pipe_gas'")),
+   list(c("'lng_liq'", "'termX_lng'")),
+   list(c("'lng_gas'", "'termX_lng'")),
+   list(c("'lng_ves'", "'vess_lng'")),
+   list(c("'coal_ves'", "'vess_coal'")),
+   list(c("vm_budgetTradeX", "!! vm_budgetTradeX")),
+   list(c("vm_budgetTradeM", "!! vm_budgetTradeM"))
+ )
+ levs_manipulateThis <- c(levs_manipulateThis, manipulate_tradesets)
+ margs_manipulateThis <- c(margs_manipulateThis, manipulate_tradesets)
+ fixings_manipulateThis <- c(fixings_manipulateThis, manipulate_tradesets)
+
+ # because of https://github.com/remindmodel/remind/pull/800
+ if (cfg$gms$cm_transpGDPscale != "on") {
+   levs_manipulateThis <- c(levs_manipulateThis, list(c("q35_transGDPshare.M", "!! q35_transGDPshare.M")))
+   margs_manipulateThis <- c(margs_manipulateThis, list(c("q35_transGDPshare.M", "!! q35_transGDPshare.M")))
+   fixings_manipulateThis <- c(fixings_manipulateThis, list(c("q35_transGDPshare.M", "!! q35_transGDPshare.M")))
+ }
+
+ # renamed because of https://github.com/remindmodel/remind/pull/848, 1066
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("vm_forcOs.L", "!!vm_forcOs.L")),
+   list(c("v32_shSeEl.L", "!!v32_shSeEl.L"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("vm_forcOs.M", "!!vm_forcOs.M")),
+   list(c("v32_shSeEl.M", "!!v32_shSeEl.M"))
+ )
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("vm_forcOs.FX", "!!vm_forcOs.FX")),
+   list(c("v32_shSeEl.FX", "!!v32_shSeEl.FX"))
+ )
+
+ # filter out deprecated regipol items
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("v47_emiTarget.L", "!!v47_emiTarget.L")),
+   list(c("v47_emiTargetMkt.L", "!!v47_emiTargetMkt.L")),
+   list(c("vm_taxrevimplEnergyBoundTax.L", "!!vm_taxrevimplEnergyBoundTax.L"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("v47_emiTarget.M", "!!v47_emiTarget.M")),
+   list(c("v47_emiTargetMkt.M", "!!v47_emiTargetMkt.M")),
+   list(c("q47_implFETax.M", "!!q47_implFETax.M")),
+   list(c("q47_emiTarget_mkt_netCO2.M", "!!q47_emiTarget_mkt_netCO2.M")),
+   list(c("q47_emiTarget_mkt_netGHG.M", "!!q47_emiTarget_mkt_netGHG.M")),
+   list(c("q47_emiTarget_netCO2.M", "!!q47_emiTarget_netCO2.M")),
+   list(c("q47_emiTarget_netCO2_noBunkers.M", "!!q47_emiTarget_netCO2_noBunkers.M")),
+   list(c("q47_emiTarget_netCO2_noLULUCF_noBunkers.M", "!!q47_emiTarget_netCO2_noLULUCF_noBunkers.M")),
+   list(c("q47_emiTarget_netGHG.M", "!!q47_emiTarget_netGHG.M")),
+   list(c("q47_emiTarget_netGHG_noBunkers.M", "!!q47_emiTarget_netGHG_noBunkers.M")),
+   list(c("q47_emiTarget_netGHG_noLULUCF_noBunkers.M", "!!q47_emiTarget_netGHG_noLULUCF_noBunkers.M")),
+   list(c("q47_emiTarget_netGHG_LULUCFGrassi_noBunkers.M", "!!q47_emiTarget_netGHG_LULUCFGrassi_noBunkers.M")),
+   list(c("q47_emiTarget_grossEnCO2.M", "!!q47_emiTarget_grossEnCO2.M")),
+   list(c("q47_emiTarget_mkt_netCO2.M", "!!q47_emiTarget_mkt_netCO2.M")),
+   list(c("q47_emiTarget_mkt_netCO2_noBunkers.M", "!!q47_emiTarget_mkt_netCO2_noBunkers.M")),
+   list(c("q47_emiTarget_mkt_netCO2_noLULUCF_noBunkers.M", "!!q47_emiTarget_mkt_netCO2_noLULUCF_noBunkers.M")),
+   list(c("q47_emiTarget_mkt_netGHG.M", "!!q47_emiTarget_mkt_netGHG.M")),
+   list(c("q47_emiTarget_mkt_netGHG_noBunkers.M", "!!q47_emiTarget_mkt_netGHG_noBunkers.M")),
+   list(c("q47_emiTarget_mkt_netGHG_noLULUCF_noBunkers.M", "!!q47_emiTarget_mkt_netGHG_noLULUCF_noBunkers.M")),
+   list(c("q47_emiTarget_mkt_netGHG_LULUCFGrassi_noBunkers.M", "!!q47_emiTarget_mkt_netGHG_LULUCFGrassi_noBunkers.M")),
+   list(c("qm_balFeAfterTax.M", "!!qm_balFeAfterTax.M")),
+   list(c("q47_implicitQttyTargetTax.M", "!!q47_implicitQttyTargetTax.M")),
+   list(c("q47_implEnergyBoundTax.M", "!!q47_implEnergyBoundTax.M")),
+   list(c("vm_taxrevimplEnergyBoundTax.M", "!!vm_taxrevimplEnergyBoundTax.M"))
+ )
+
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("v47_emiTarget.FX", "!!v47_emiTarget.FX")),
+   list(c("v47_emiTargetMkt.FX", "!!v47_emiTargetMkt.FX")),
+   list(c("vm_taxrevimplEnergyBoundTax.FX", "!!vm_taxrevimplEnergyBoundTax.FX"))
+ )
+
+ # renamed because of https://github.com/remindmodel/remind/pull/1106
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("v21_taxrevBioImport.L", "!!v21_taxrevBioImport.L"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("v21_taxrevBioImport.M", "!!v21_taxrevBioImport.M")),
+   list(c("q21_taxrevBioImport.M", "!!q21_taxrevBioImport.M")),
+   list(c("q30_limitProdtoHist.M", "!!q30_limitProdtoHist.M"))
+ )
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("v21_taxrevBioImport.FX", "!!v21_taxrevBioImport.FX"))
+ )
+
+ # renamed because of https://github.com/remindmodel/remind/pull/1128
+ levs_manipulateThis <- c(
+   levs_manipulateThis,
+   list(c("v_emiTeDetailMkt.L", "!!v_emiTeDetailMkt.L")),
+   list(c("v_emiTeMkt.L", "!!v_emiTeMkt.L"))
+ )
+ margs_manipulateThis <- c(
+   margs_manipulateThis,
+   list(c("v_emiTeDetailMkt.M", "!!v_emiTeDetailMkt.M")),
+   list(c("v_emiTeMkt.M", "!!v_emiTeMkt.M"))
+ )
+ fixings_manipulateThis <- c(
+   fixings_manipulateThis,
+   list(c("v_emiTeDetailMkt.FX", "!!v_emiTeDetailMkt.FX")),
+   list(c("v_emiTeMkt.FX", "!!v_emiTeMkt.FX"))
+ )
   # Include fixings (levels) and marginals in full.gms at predefined position
   # in core/loop.gms.
   full_manipulateThis <- c(
